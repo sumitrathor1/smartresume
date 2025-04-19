07bf89cd8d4aec07258e3b293f9cc8bd981b11f2
Sumit Rathor <sumitrathor142272@gmail.com>
2025-04-19 20:14:45 +0530

    git js

--- create-resume.php
+++ create-resume.php
@@ -13,7 +13,9 @@ if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_email'])) {
 <head>
     <meta charset="UTF-8">
     <title>Create Resume - SmartResume</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-SgOJa3DmI69IUzQ2PVdRZhwQ+dy64/BUtbMJw1MZ8t5HZApcHrRKUc4W0kG879m7" crossorigin="anonymous">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
     <link rel="stylesheet" href="assets/css/dashboard_main.css">
 </head>
 
@@ -62,6 +64,9 @@ if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_email'])) {
     </main>
     <?php include "./assets/pages/_dashboard-footer.php" ?>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous">
+    </script>
 </body>
 
 </html>
--- dashboard.php
+++ dashboard.php
@@ -64,7 +64,6 @@ if (!isset($_SESSION['user_id']) || !isset($_SESSION['user_email'])) {
 
 
     <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/css/bootstrap.min.css" rel="stylesheet">
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.5/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-k6d4wzSIapyDyv1kpU366/PK5hCdSbCRGRCMv+eplOQJWyd1fbcAu9OCUj5zNLiq" crossorigin="anonymous">
     </script>
