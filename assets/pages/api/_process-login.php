906e3e144ae2f8a42d68868aeb62cb44868e3844
Sumit Rathor <sumitrathor142272@gmail.com>
2025-04-19 19:30:29 +0530

    C:\xampp\htdocs\Resume Builder\assets\pages\api\_process-login.php

--- assets/pages/api/_process-login.php
+++ assets/pages/api/_process-login.php
@@ -23,6 +23,7 @@ if ($result->num_rows === 1) {
     if (password_verify($password, $user['password'])) {
         $_SESSION['user_id'] = $user['id'];
         $_SESSION['user_email'] = $user['email'];
+        $_SESSION['user_name'] = $user['name'];
         echo "success";
     } else {
         echo "Incorrect password.";
